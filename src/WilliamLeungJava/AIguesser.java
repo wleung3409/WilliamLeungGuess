fad3633faebe211cd7a1b763fe2d6557a2a85108
BT_1N3_12 <wleung3409@bths.edu>
2018-10-11 08:43:21 -0400

    Commit

--- src/WilliamLeungJava/AIguesser.java
+++ src/WilliamLeungJava/AIguesser.java
@@ -1,13 +1,38 @@
 package WilliamLeungJava;
 
+import java.util.Scanner;
+
 public class AIguesser {
-    public static void AIguesser(int guess){
+    public static void AIguessLinear(int guess){
         int AIguess = 0;
-        while (AIguess<guess){
-            AIguess= AIguess +10;
+       for(int x=0;guess!= AIguess;x++){
+           AIguess = AIguess + 1;
+           }
+           System.out.println("Your number is "+ AIguess +" and it took me "+AIguess +" tries");
+    }
+    public static void AIguessBinary(int guess,int min,int max){
+        int AIguess = 50;
+        for(int x=0;guess!=AIguess;x++){
+            if (AIguess<guess){
+                min = AIguess;
+                AIguess = max - (min/2);
+            }
+            if (AIguess>guess){
+                max = AIguess;
+                AIguess = 
+            }
+
         }
-        if (AIguess==guess){
-            System.out.println("Easy");
+    }
+    public static void RandomGuess(String[] args) {
+        // write your code here
+        double x = Math.random();
+        int random = (int) (x * 11);
+        System.out.println("Guess a number 1-10");
+        Scanner input = new Scanner(System.in);
+        while (!(input.nextInt() == random)) {
+            System.out.println("Try again");
         }
+        System.out.println("Congrats you did it!");
     }
 }
